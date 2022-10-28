ef2fbed18e069733a9d1e639515a03efa57ceb7f
daniltop3309 <114923511+daniltop3309@users.noreply.github.com>
2022-10-28 15:12:05 +0300

    Add files via upload

---
+++ assets/js/main2.js
@@ -0,0 +1,91 @@
+// let arr = ["Привет", ",", " Мир", "!"]
+// console.log(arr.join(""))
+
+
+// let arr = ["Привет", ",", " Мир", "!"];
+// let text = (arr.join(""));
+// console.log(text)
+
+
+// let arr = ["Привет", " Мир", "!"];
+// arr[0] = "Пока,";
+// console.log(arr.join(""))
+
+
+// let obj = {'Коля':'10', 'Даниль':'5', 'Петя':'2'};
+// console.log(obj["Коля"])
+// console.log(obj["Петя"])
+
+
+// var arr = {
+//     "ru":["голубой", "красный", "зеленый"],
+//     "en":["blue", "red", "green"]
+// };
+// alert(arr['ru'][1])
+
+
+// let arr = ['a', 'b', 'c'];
+// alert(arr.join(""))
+
+
+// let arr = ['a', 'b', 'c'];
+// alert(arr.join(" "))
+
+
+// let arr = ['a', 'b', 'c', "d"];
+// alert(`${arr[0]} + ${arr[1]}, ${arr[2]} + ${arr[3]}`);
+
+
+// let arr = [2, 5, 3, 9];
+// let result = 2 * 5 + 3 * 9;
+// alert(result)
+
+
+// var obj = { a: 1, b: 2, c: 3 };
+// alert(obj['c']);
+// alert(obj.c);
+
+
+// var obj = { Коля: '1000', Вася: '500', Петя: '200' };
+// alert(obj.Петя);
+// alert(obj.Коля);
+
+
+
+// var obj = {1: 'пн', 2: 'вт', 3: 'ср', 4: 'чт', 5: 'пт', 6: 'сб', 7: 'вс'}; 
+// alert(obj[5]); 
+ 
+
+// var obj = {1: 'пн', 2: 'вт', 3: 'ср', 4: 'чт', 5: 'пт', 6: 'сб', 7: 'вс'}; 
+// let day = 1; 
+// alert(obj[day]);
+
+
+// let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+// alert(arr[1][0]);
+
+
+// let arr = {
+//     'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+//     'en': ['Monday', 'Tuesday', 'Wednesday', 'Thirsday', 'Friday', 'Saturday', 'Sunday'],
+// };
+// alert(arr.ru[0]);
+// alert(arr.en[2]);
+
+
+// let arr = {
+//     'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+//     'en': ['Monday', 'Tuesday', 'Wednesday', 'Thirsday', 'Friday', 'Saturday', 'Sunday'],
+//     };
+// let lang = 'ru';
+// let day = 1;
+// alert(arr[lang][day]);
+
+
+// let arr = { 
+//     'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'], 
+//     'en': ['Monday', 'Tuesday', 'Wednesday', 'Thirsday', 'Friday', 'Saturday', 'Sunday'], 
+// } 
+// let lang = 'ru'; 
+// let day = 5; 
+// alert(arr[lang][day]);
